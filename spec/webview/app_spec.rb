4ab705385af1f532ab2a5f1c0cd9b193462e5068
jiangzhi.xie <xiejiangzhi@gmail.com>
2019-05-10 12:26:14 +0800

    Fix RSpec, don't check alive when is not GUI environment

--- spec/webview/app_spec.rb
+++ spec/webview/app_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Webview::App do
       expect(subject.open('http://localhost:1234/asdf')).to eql(true)
       sleep 0.5
       ap = subject.app_process
-      expect(ap.alive?).to eql(true)
+      expect(ap.alive?).to eql(true) if $gui_env
       expect {
         subject.close
       }.to change { ap.alive? }.to(false)
@@ -27,7 +27,7 @@ RSpec.describe Webview::App do
       expect(subject.open('http://localhost:4321/aaa')).to eql(true)
       sleep 0.5
       ap = subject.app_process
-      expect(ap.alive?).to eql(true)
+      expect(ap.alive?).to eql(true) if $gui_env
       expect {
         subject.close
       }.to change { ap.alive? }.to(false)
